eaedbeb07bcf894295d6a14bdf7409c33d705b85
Jimmy <247057911@qq.com>
2018-06-13 17:36:45 +0800

    初步做了个电梯的交互
    导入standard asserts,利用携程，Raycast在FPSController实现物品交互提示，按键交互

---
+++ room2/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/openDoor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour {
+    [SerializeField]
+    public Vector3 dPos;
+
+    bool isInteractive = false;
+
+    bool isOpen = false;
+
+    bool stillOpen = false;
+
+    public void notifyInteractive()
+    {
+        isInteractive = true;
+    }
+
+    void Update()
+    {
+        if (isInteractive)
+        {
+            if (!stillOpen)
+            {
+                StartCoroutine(opendoor());
+            }
+            isInteractive = false;
+        }
+    }
+
+    IEnumerator opendoor()
+    {
+        stillOpen = true;
+        Vector3 pos = dPos;
+        pos.Scale(new Vector3(0.2f, 0.2f, 0.2f));
+        float time = Time.deltaTime * 5;
+        while (time > 0)
+        {
+            if (isOpen)
+                transform.position -= pos;
+            else
+                transform.position += pos;
+
+            time -= Time.deltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        isOpen = !isOpen;
+        stillOpen = false;
+    }
+}
