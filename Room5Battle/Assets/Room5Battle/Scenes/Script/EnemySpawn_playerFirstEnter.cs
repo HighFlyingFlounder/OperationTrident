d4a1266d3da49d6c983b46391a65abb5321a9d1a
CHINESE JIGE <836613859@qq.com>
2018-06-13 17:49:58 +0800

    第一波敌人生成

--- Room5Battle/Assets/Room5Battle/Scenes/Script/EnemySpawn_playerFirstEnter.cs
+++ Room5Battle/Assets/Room5Battle/Scenes/Script/EnemySpawn_playerFirstEnter.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawn_playerFirstEnter : MonoBehaviour {
+public class EnemySpawn_playerFirstEnter : MonoBehaviour
+{
+    //private List<GameObject> m_EnemyList;
+
+    [SerializeField]
+    private GameObject m_EnemyPrefab1;
+
+    [SerializeField]
+    private Transform m_EnemySpawnPos_OnGround1;
 
 	// Use this for initialization
 	void Start () {
@@ -13,9 +21,27 @@ public class EnemySpawn_playerFirstEnter : MonoBehaviour {
 	void Update () {
 		
 	}
-
-    private void OnTriggerEnter(Collider other)
+    
+    void OnTriggerEnter(Collider collider)
     {
-        
+        //生成敌人（出生pos用serializeField绑定在unity的对象里面）
+        const int c_enemyNum = 5;
+
+        //敌人在某个位置隔壁一定box范围内生成
+        const float c_spawnboxWidth = 5.0f;
+        const float c_spawnboxHeight = 1.0f;
+        const float c_spawnboxDepth = 5.0f;
+        for (int i = 0; i < c_enemyNum; ++i)
+        {
+            float x_offset = Random.Range(-c_spawnboxWidth, c_spawnboxWidth);
+            float y_offset = Random.Range(-c_spawnboxHeight, c_spawnboxHeight);
+            float z_offset = Random.Range(-c_spawnboxDepth, c_spawnboxDepth);
+            Instantiate(
+                m_EnemyPrefab1, 
+                m_EnemySpawnPos_OnGround1.position + new Vector3(x_offset,y_offset,z_offset), 
+                m_EnemySpawnPos_OnGround1.rotation);
+        }
+
+        Destroy(this.gameObject);
     }
 }
