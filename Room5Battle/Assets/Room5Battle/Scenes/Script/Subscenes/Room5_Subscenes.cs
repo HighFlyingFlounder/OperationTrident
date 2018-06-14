97b817e77e5176fb597b5e72bbead3473ba82afb
CHINESE JIGE <836613859@qq.com>
2018-06-14 16:46:59 +0800

    用回SubsceneController 有点小坑解决了
    1.SubsceneController要挂上一个GameObject,其他要引用到的Subscene具体实现script也要挂上这个GameObject,不然GetComponent找不到
    2.Subscene::isTransitionTriggered默认返回true,其实还是false好啊,不然忘了重写就一次过跳到最后了

---
+++ Room5Battle/Assets/Room5Battle/Scenes/Script/Subscenes/Room5_Subscenes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Room5Battle
+{
+    public enum GameState
+    {
+        START,//玩家刚进，有少量敌人生成，,准备开始冷却托卡马克之心
+        COUNTING_DOWN_5MIN,//核心开始冷却,倒计时,开始生成新的敌人
+        COUNTING_DOWN_3MIN,
+        COUNTING_DOWN_1MIN,
+        ESCAPING
+    }
+
+    public class Room5_Subscenes : MonoBehaviour
+    {
+        public  SubsceneController m_SubsceneController;
+
+        // Use this for initialization
+        void Start()
+        {
+            m_SubsceneController.addSubscene((int)GameState.START, "Subscene_Start");
+            m_SubsceneController.addSubscene((int)GameState.COUNTING_DOWN_5MIN, "Subscene_5min");
+            m_SubsceneController.addSubscene((int)GameState.COUNTING_DOWN_3MIN, "Subscene_3min");
+
+            m_SubsceneController.setInitialSubscene((int)GameState.START);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
---
+++ Room5Battle/Assets/Room5Battle/Scenes/Script/Subscenes/Subscene_Start.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Room5Battle
+{
+    public class Subscene_Start : Subscene
+    {
+        //托卡马克之心是否已启动冷却（要对着控制台按住F十秒）
+        private bool m_IsTokamakeStartToCoolDown;
+
+        //对着控制台按着F
+        private bool m_isControlPanelPressingKeyF=false;
+        private float m_ControlPanelKeyFPressingTime = 0.0f;
+
+        public override bool isTransitionTriggered()
+        {
+            return m_IsTokamakeStartToCoolDown;
+        }
+
+        //@brief 返回下一个子场景
+        public override int GetNextSubscene()
+        {
+            return (int)GameState.COUNTING_DOWN_5MIN;
+        }
+
+        //@brief 子场景的初始化，可以在初始化阶段将所有元素的行为模式改为此状态下的逻辑
+        public override void onSubsceneInit()
+        {
+            Debug.Log("Entering Subscene:start");
+        }
+
+        //@brief 善后工作
+        public override void onSubsceneDestory()
+        {
+            Debug.Log("Leaving Subscene:start");
+        }
+
+        /***************************************************
+         *                     Subscene's controller
+         * *************************************************/
+        private void  Update()
+        {
+            Debug.Log(m_ControlPanelKeyFPressingTime.ToString());
+            //摄像机中心发出的射线
+            Vector3 centerCoordPixel = new Vector3(Camera.main.pixelWidth/2, Camera.main.pixelHeight/2);
+            Ray viewRay = Camera.main.ScreenPointToRay(centerCoordPixel);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(viewRay, out hitInfo, 2f))
+            {
+                //如果玩家看着控制台
+                if (hitInfo.transform.CompareTag("ControlPanel"))
+                {
+                    if (Input.GetKeyDown(KeyCode.F))
+                    {
+                        m_isControlPanelPressingKeyF = false;
+                        m_ControlPanelKeyFPressingTime = 0;
+                    }
+                    else
+                    {
+                        m_isControlPanelPressingKeyF = true;
+                        m_ControlPanelKeyFPressingTime += Time.deltaTime;
+                    }
+                }
+                else
+                {
+                    m_isControlPanelPressingKeyF = false;
+                }
+            }
+
+            //按住F 10s就可以了
+            if(m_ControlPanelKeyFPressingTime>2.0f)
+            {
+                //isTransitionTriggered= true, 开始冷却
+                Debug.Log("按完了啊");
+                m_IsTokamakeStartToCoolDown = true;
+            }
+        }
+
+        private void OnGUI()
+        {
+
+        }
+
+    }
+}
