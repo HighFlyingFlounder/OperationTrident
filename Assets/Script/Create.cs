510a63e6ba2269a2c15cdb15c754b4f10c6ad097
yahoohya <yahoohyax@gmail.com>
2018-06-14 17:45:25 +0800

    真*首次推送

---
+++ Assets/Script/Create.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create : MonoBehaviour {
+    [SerializeField]
+    //要生成的物体
+    public GameObject CreateObject;
+    //要生成的数量
+    public int number;
+    //生成速度
+    public float time;
+    //是否开始生成物体
+    public bool isStart;
+
+    //协程
+    private IEnumerator coroutine;
+    //维护一个物体列表
+    public List<GameObject> list;
+
+    // Use this for initialization
+    void Start () {
+        //一秒执行一次
+        coroutine = WaitAndPrint(time);
+
+        number = 10;
+        time = 1;
+        isStart = false;
+        list = new List<GameObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //判断何时生成
+        if (isStart)
+        {
+            //开始生成
+            StartCoroutine(coroutine);
+            isStart = false;
+        }
+
+        if (list.Count % number == 0)
+        {
+            //停止生成器
+            StopCoroutine(coroutine);
+            //留种
+            coroutine = WaitAndPrint(time);
+        }
+    }
+
+    //协程coroutine的存在，配合下面的yield return回调，这段代码将被1s执行一次
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        for (int i = 0; i < number;)
+        {
+            //生成物体
+            GameObject o = Instantiate(CreateObject, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
+            o.name = list.Count.ToString();
+            list.Add(o);
+            i++;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    public void Operate()
+    {
+        isStart = true;
+    }
+}
---
+++ Assets/Script/SceneController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OperationTrident.Elevator;
+
+namespace OperationTrident.Elevator {
+    public class SceneController : MonoBehaviour
+    {
+        [SerializeField]
+        public GameObject EnemyPosPre;
+        //出生点的位置
+        public List<Vector3> ePos = new List<Vector3>();
+        //电梯开关的预设
+        public GameObject buttonPre;
+        //button的位置
+        public Vector3 ButtonPosition;
+
+
+        private enum ElevatorState { Initing, FindingButton, Fighting, End };
+        private GameObject button;
+        private ElevatorState state;
+        //敌人出生点
+        private List<GameObject> EnemyPos;
+
+        //private void Awake()
+        //{
+        //    Messenger<int>.AddListener(GameEvent.BUTTON_GOT, OnButtonGot);
+        //}
+
+        //private void Destroy()
+        //{
+        //    Messenger<int>.RemoveListener(GameEvent.BUTTON_GOT, OnButtonGot);
+        //}
+
+        // Use this for initialization
+        void Start()
+        {
+            state = ElevatorState.Initing;
+            EnemyPos = new List<GameObject>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            switch (state)
+            {
+                case ElevatorState.Initing:
+                    //初始化BUTTON
+                    button = GameObject.Instantiate(buttonPre);
+                    button.transform.localPosition = ButtonPosition;
+
+                    for (int i = 0; i < ePos.Count; i++)
+                    {
+                        GameObject temp = GameObject.Instantiate(EnemyPosPre);
+                        temp.transform.localPosition = ePos[i];
+                        EnemyPos.Add(temp);
+                    }
+
+                    state = ElevatorState.FindingButton;
+                    break;
+
+                case ElevatorState.FindingButton:
+                    break;
+
+                case ElevatorState.Fighting:
+                    foreach (GameObject enemy in EnemyPos)
+                    {
+                        enemy.SendMessage("Operate", SendMessageOptions.DontRequireReceiver);
+                    }
+                    state = ElevatorState.End;
+                    break;
+                case ElevatorState.End:
+                    break;
+            }
+        }
+
+
+        private void Operate()
+        {
+
+            if (state == ElevatorState.FindingButton)
+            {
+                button.SendMessage("Operate", SendMessageOptions.DontRequireReceiver);
+                state = ElevatorState.Fighting;
+            }
+        }
+
+
+    }
+}
