a48aeec37951fb6d913db470efef265f09ee3eb1
WindyIce <windyicec@gmail.com>
2018-06-14 15:43:20 +0800

    ai模块小改
    没错，现在场景一开始会先全部生成AI敌人。然后再慢慢生成

--- Assets/Scripts/AISceneController.cs
+++ Assets/Scripts/AISceneController.cs
@@ -15,6 +15,8 @@ namespace OperationTrident.Room1
         new Vector3(-50.95f, 2.755f, 21.0f); 
         new Vector3(-27.1f, 2.755f, 8.858f); 
         new Vector3(-27.1f, 5.784f, 13.2f); 
+        new Vector3(5.79f, 1.985f, 25.23f);
+        new Vectore(-44.271f, 2.281f, -8.47f);
         */
 
         // 敌人机器人的预设
@@ -26,18 +28,24 @@ namespace OperationTrident.Room1
         public float enemyCreateSpeed;
         // 场景中最大的敌人数量
         public int enemyMaxNum = 10;
+        // 场景中当前敌人的数量
+        [SerializeField]
+        private int nowEnemyNum;
 
         // Use this for initialization
         void Start()
         {
             enemysList = new List<GameObject>();
+            enemyCreateAllAtPreSetPosition();
+            nowEnemyNum = enemysList.Count;
             StartCoroutine(EnemyCreateRountine());
         }
 
         // Update is called once per frame
         void Update()
         {
-            for(int i=0;i<enemysList.Count;i++)
+            nowEnemyNum = enemysList.Count;
+            for (int i=0;i<enemysList.Count;i++)
             {
                 ReactiveTarget rt = enemysList[i].GetComponent<ReactiveTarget>();
                 if (rt.Dead)
@@ -53,7 +61,7 @@ namespace OperationTrident.Room1
             // 这里暂时是每时每刻都生成？@！#￥
             while (true)
             {
-                if (enemyCreateList.Count <= enemyMaxNum)
+                if (nowEnemyNum < enemyMaxNum)
                 {
                     enemyCreate();
                 }
@@ -68,6 +76,17 @@ namespace OperationTrident.Room1
             nowEnemy.transform.localPosition = enemyCreateList[UnityEngine.Random.Range(0, enemyCreateList.Count)];
             enemysList.Add(nowEnemy);
 
+        }
+
+        // 敌人一次性全部产生，在指定的位置各生成一个
+        private void enemyCreateAllAtPreSetPosition()
+        {
+            foreach(var a in enemyCreateList)
+            {
+                GameObject nowEnemy = Instantiate(enemyPrefab) as GameObject;
+                nowEnemy.transform.localPosition = a;
+                enemysList.Add(nowEnemy);
+            }
         }
     }
 }
