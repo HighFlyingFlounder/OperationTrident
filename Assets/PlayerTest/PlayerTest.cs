46f5d779a7958d6b584dcfee26a953af0c7e42a1
DengJunYuan <540997806@qq.com>
2018-06-21 17:20:54 +0800

    PlayerTest和PlayerTest2场景切换

---
+++ Assets/PlayerTest/PlayerTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerTest : MonoBehaviour {
+    private bool arrived = false;
+	// Use this for initialization
+	void Start () {
+        NetMgr.srvConn.msgDist.AddListener("Result", RecvResult);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!arrived)
+          SendSpaceArriveEnd();
+    }
+
+    public void SendSpaceArriveEnd()
+    {
+        ProtocolBytes proto = new ProtocolBytes();
+        proto.AddString("SpaceArriveEnd");
+        //位置和旋转
+        NetMgr.srvConn.Send(proto);
+    }
+
+    public void RecvResult(ProtocolBase protocol)
+    {
+        //解析协议
+        int start = 0;
+        ProtocolBytes proto = (ProtocolBytes)protocol;
+        string protoName = proto.GetString(start, ref start);
+        int isWin = proto.GetInt(start, ref start);
+
+        Debug.Log("111111111111");
+        SceneManager.LoadScene("PlayerTest2", LoadSceneMode.Single);
+
+        //取消监听
+        NetMgr.srvConn.msgDist.DelListener("Result", RecvResult);
+    }
+}
