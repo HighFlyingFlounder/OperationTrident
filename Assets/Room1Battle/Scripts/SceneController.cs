107b8fe6f789ce3f88cc1eb287b094a7c5d66ee8
Peiwei Chang <windyicec@gmail.com>
2018-06-28 15:20:43 +0800

    GUI加了注释，可以动态添加Sync_Script，子弹使用弹道和生成的方式 (#59)
    * 所有函数加了注释
    
    所有函数都有C#标准注释
    
    * Room1正确生成player.prefab，但未添加场景特定脚本
    
    * 客户端本地玩家的逻辑可以正确同步玩家位置，但还未同步场景状态
    
    * sync_scripts变成了List，门的同步没成功
    
    Room1 增加了网络同步模块
    
    * guiutil加了函数，场景变成新的
    
    
    * 场景文件更改
    
    
    * 正确同步射击动作，但未进行伤害同步
    
    * GUIUtil改了时间，改了同步规范
    
    
    * 开始场景进入StoryBoard2，结束后进入SpaceBattle
    
    1：删掉了测试场景和StartScene的光照数据
    2：开始场景进入StoryBoard2，结束后进入SpaceBattle
    
    * 完善了下GUI
    
    * 做完了Bullet
    
    子弹实现，有自己的弹道
    
    * 解决了任务目标位置显示异常的问题
    
    
    * 修复了GUIUtil的小Bug
    
    多了给定时间数组显示的时间
    
    * 加了很多特效

--- Assets/Room1Battle/Scripts/SceneController.cs
+++ Assets/Room1Battle/Scripts/SceneController.cs
@@ -310,24 +310,27 @@ namespace OperationTrident.Room1
         private int subtitleIndex = 0;
         void OnGUI()
         {
-            //GUIUtil.DisplaySubtitlesInGivenGrammar(
-            //    subtitlesToDisplay,
-            //    Camera.main,
-            //    fontSize: 16,
-            //    subtitleRatioHeight: 0.9f,
-            //    secondOfEachWord: 0.2f,
-            //    secondBetweenLine: 4.0f);
-
-            string[] toD = { "^w你好，^r一王^w，我是^b WindyIce", "^w你好，^r鸡王^w，我是^b WindyIce" };
-            float[] a1 = { 10.0f, 5.0f };
-            float[] a2 = { 5.0f, 10.0f };
-            GUIUtil.DisplaySubtitlesInGivenGrammarWithTimeStamp(
-                toD,
+            GUIUtil.DisplaySubtitlesInGivenGrammar(
+                subtitlesToDisplay,
                 Camera.main,
                 fontSize: 16,
                 subtitleRatioHeight: 0.9f,
-                secondsOfEachLine:a1,
-                secondBetweenLine: a2);
+                secondOfEachWord: 0.2f,
+                secondBetweenLine: 4.0f);
+
+            //string[] toD = {
+            //    "^w你好，^r一王^w，我是^b WindyIce",
+            //    "^w你好，^r鸡王^w，我是^b WindyIce" ,
+            //    "^w我们都要取回^b托卡马克之心" };
+            //float[] a1 = { 10.0f, 5.0f ,10.0f};
+            //float[] a2 = { 5.0f, 10.0f ,5.0f};
+            //GUIUtil.DisplaySubtitlesInGivenGrammarWithTimeStamp(
+            //    toD,
+            //    Camera.main,
+            //    fontSize: 16,
+            //    subtitleRatioHeight: 0.9f,
+            //    secondsOfEachLine:a1,
+            //    secondBetweenLine: a2);
         }
 
         public void RecvData(SyncData data)
