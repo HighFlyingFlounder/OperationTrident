1c54b2790139b926e94ccc8aa27e21663c8aa316
CHINESE JIGE <836613859@qq.com>
2018-06-29 18:01:07 +0800

    改一下宇宙天空球/光源

---
+++ Assets/EndingScene/Scripts/UniverseLightSourceFlare.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniverseLightSourceFlare : MonoBehaviour {
+
+    public LensFlare m_LensFlares;
+    public Light m_DirectionalLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //在平行光的反向位置
+        Vector3 offsetFromCam = -10000.0f * m_DirectionalLight.transform.forward;
+        m_LensFlares.transform.position = Camera.current.transform.position + offsetFromCam;
+
+        //调一下LensFlares的亮度（正对着最亮）
+        Vector3 camLookat = Camera.current.transform.forward;
+        float dotP = Vector3.Dot(camLookat.normalized, offsetFromCam.normalized);
+        if(dotP>0.0f)
+        {
+            m_LensFlares.brightness = dotP * 3.0f;
+        }
+
+
+    }
+}
