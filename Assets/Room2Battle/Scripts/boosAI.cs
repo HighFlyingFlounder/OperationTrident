464959927b33107e17ab42c62e07ed68ae8bd9b5
Jimmy <247057911@qq.com>
2018-06-29 17:23:05 +0800

    导弹发射

--- Assets/Room2Battle/Scripts/boosAI.cs
+++ Assets/Room2Battle/Scripts/boosAI.cs
@@ -6,14 +6,62 @@ public class boosAI : MonoBehaviour {
     [SerializeField]
     protected GameObject[] Missiles;
 
-    int i = 0;
+    [SerializeField]
+    protected Transform[] pos;
+
+    [SerializeField]
+    protected Transform target;
+
+    [SerializeField]
+    protected float radius = 5.0f;
+
+    //导弹齐射完毕与否
+    protected bool isShotDone = true;
 
     void Update()
     {
-        if (i < 10)
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            Instantiate(Missiles[Random.Range(0, Missiles.Length)], transform);
+            if (isShotDone)
+            {
+                StartCoroutine(shotTogether());
+            }
         }
-        i++;
+
     }
+    /// <summary>
+    /// 使用携程隔一段时间发一枚导弹
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator shotTogether()
+    {
+        isShotDone = false;
+        float r = radius;
+        for (int j = 0; j < 3; ++j)
+        {
+            r += 3;
+            for (int i = 0; i < pos.Length; ++i)
+            {
+                Vector3 pos_ = target.position;
+                pos_.x += Random.Range(-r, r);
+                pos_.y += Random.Range(-r, r);
+
+                //Quaternion targetRotation = Quaternion.LookRotation(target.position - pos[i].position);
+
+
+                //pos[i].rotation = targetRotation;
+                Transform p = pos[i];   
+                p.LookAt(pos_);
+                Quaternion ro = p.rotation;
+
+                p.eulerAngles = new Vector3(-90,p.eulerAngles.y,0);
+                Instantiate(Missiles[Random.Range(0, Missiles.Length)], p.position,ro);
+              
+
+                yield return new WaitForSeconds(Random.Range(1.0f,2.0f));
+            }
+        }
+        isShotDone = true;
+    }
+
 }
