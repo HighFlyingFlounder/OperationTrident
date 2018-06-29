55600792d329b86aa266ed6eb9e7e5c0956a2f23
CHINESE JIGE <836613859@qq.com>
2018-06-29 20:48:20 +0800

    lens flare的小修改

--- Assets/EndingScene/Scripts/UniverseLightSourceFlare.cs
+++ Assets/EndingScene/Scripts/UniverseLightSourceFlare.cs
@@ -13,17 +13,18 @@ public class UniverseLightSourceFlare : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void OnPreRender() {
         //在平行光的反向位置
-        Vector3 offsetFromCam = -10000.0f * m_DirectionalLight.transform.forward;
-        m_LensFlares.transform.position = Camera.current.transform.position + offsetFromCam;
+        //Vector3 offsetFromCam = -10000.0f * m_DirectionalLight.transform.forward;
+        //m_LensFlares.transform.position = Camera.current.transform.position + offsetFromCam;
 
         //调一下LensFlares的亮度（正对着最亮）
         Vector3 camLookat = Camera.current.transform.forward;
-        float dotP = Vector3.Dot(camLookat.normalized, offsetFromCam.normalized);
+        float dotP = Vector3.Dot(camLookat.normalized, -m_DirectionalLight.transform.forward.normalized);
         if(dotP>0.0f)
         {
-            m_LensFlares.brightness = dotP * 3.0f;
+            const float maxBrightness = 2.0f;
+            m_LensFlares.brightness = dotP * maxBrightness;
         }
 
 
