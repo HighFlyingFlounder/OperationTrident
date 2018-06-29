ed664ba54904846112a96f1726d411a5d430034a
Jimmy <247057911@qq.com>
2018-06-29 21:01:40 +0800

    差一点，差一个坐标转换

--- Assets/Room2Battle/Scripts/boosAI.cs
+++ Assets/Room2Battle/Scripts/boosAI.cs
@@ -2,66 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class boosAI : MonoBehaviour {
-    [SerializeField]
-    protected GameObject[] Missiles;
-
-    [SerializeField]
-    protected Transform[] pos;
-
-    [SerializeField]
-    protected Transform target;
-
-    [SerializeField]
-    protected float radius = 5.0f;
-
-    //导弹齐射完毕与否
-    protected bool isShotDone = true;
-
-    void Update()
+namespace room2Battle
+{
+    public class boosAI : MonoBehaviour
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        [SerializeField]
+        protected GameObject[] Missiles;
+
+        [SerializeField]
+        protected missilLauncher[] pos;
+
+        [SerializeField]
+        protected Transform target;
+
+        [SerializeField]
+        protected float radius = 5.0f;
+
+        //导弹齐射完毕与否
+        protected bool isShotDone = true;
+
+        void Update()
         {
-            if (isShotDone)
-            {
-                StartCoroutine(shotTogether());
-            }
-        }
 
-    }
-    /// <summary>
-    /// 使用携程隔一段时间发一枚导弹
-    /// </summary>
-    /// <returns></returns>
-    IEnumerator shotTogether()
-    {
-        isShotDone = false;
-        float r = radius;
-        for (int j = 0; j < 3; ++j)
+            foreach (missilLauncher rocket in pos)
+            {
+                float r = 3.0f;
+                Transform temp = target;
+                //temp.position = new Vector3(target.position.x + Random.Range(-r, r), target.position.y , target.position.z + Random.Range(-r, r));
+
+                rocket.SetTargetPostion(temp);
+                rocket.launch();
+            }
+
+
+        }
+        /*
+        /// <summary>
+        /// 使用携程隔一段时间发一枚导弹
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator shotTogether()
         {
-            r += 3;
-            for (int i = 0; i < pos.Length; ++i)
+            isShotDone = false;
+            float r = radius;
+            for (int j = 0; j < 3; ++j)
             {
-                Vector3 pos_ = target.position;
-                pos_.x += Random.Range(-r, r);
-                pos_.y += Random.Range(-r, r);
+                r += 3;
+                for (int i = 0; i < pos.Length; ++i)
+                {
+                    Vector3 pos_ = target.position;
+                    pos_.x += Random.Range(-r, r);
+                    pos_.y += Random.Range(-r, r);
 
-                //Quaternion targetRotation = Quaternion.LookRotation(target.position - pos[i].position);
+                    //Quaternion targetRotation = Quaternion.LookRotation(target.position - pos[i].position);
 
 
-                //pos[i].rotation = targetRotation;
-                Transform p = pos[i];   
-                p.LookAt(pos_);
-                Quaternion ro = p.rotation;
+                    //pos[i].rotation = targetRotation;
+                    Transform p = pos[i];   
+                    p.LookAt(pos_);
 
-                p.eulerAngles = new Vector3(-90,p.eulerAngles.y,0);
-                Instantiate(Missiles[Random.Range(0, Missiles.Length)], p.position,ro);
-              
+                    Vector3 vec = new Vector3(p.eulerAngles.x, p.eulerAngles.y, p.eulerAngles.z);
+                    Quaternion ro = p.rotation;
 
-                yield return new WaitForSeconds(Random.Range(1.0f,2.0f));
+                    p.eulerAngles = new Vector3(-90,p.eulerAngles.y,0);
+
+                    Transform t = pos[i].Find("Rocket launcher");
+
+                    t.LookAt(pos_, t.up);
+                    t.localEulerAngles = new Vector3(t.localEulerAngles.x+90, t.localEulerAngles.y, t.localEulerAngles.z);
+
+
+
+                    Instantiate(Missiles[Random.Range(0, Missiles.Length)], p.position,ro);
+
+
+                    yield return new WaitForSeconds(Random.Range(1.0f,2.0f));
+                }
             }
+            isShotDone = true;
         }
-        isShotDone = true;
+        */
     }
-
 }
---
+++ Assets/Room2Battle/Scripts/missilLauncher.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace room2Battle
+{
+    /// <summary>
+    /// 一个简单的状态机，只有发射和待发射两个状态
+    /// 通过isLaunching标记当前状态
+    /// </summary>
+
+    public class missilLauncher : MonoBehaviour
+    {
+        //导弹出口
+        [SerializeField]
+        protected Transform rocketInitPos;
+
+        //导弹预设
+        [SerializeField]
+        protected GameObject missilePrefabs;
+
+        //目标位置
+        public Transform targetTransform;
+
+        //发射角度角度
+        protected Quaternion launchQuaternion;
+
+        //摆正前发射架的rotation
+        protected Vector3 launcherEulerAfter;
+
+        //摆正前导弹的rotation
+        protected Quaternion rocketEulerAfter;
+
+        //摆正钱发射架的rotation
+        protected Vector3 launcherEulerBefore;
+
+        //摆正后导弹的rotation
+        protected Quaternion rocketEulerBefore;
+
+        //是否正在发射
+        protected bool isLaunching = false;
+
+        public void SetTargetPostion(Transform pos)
+        {
+            //不能中途修改目标
+            if(!isLaunching)
+                targetTransform = pos;
+        }
+
+        public void launch()
+        {
+            if (!isLaunching)
+            {
+                
+                Transform tempLauncherTransform = transform;
+                Transform t = transform.Find("Rocket launcher");
+                t.SetParent(tempLauncherTransform);
+
+                launcherEulerBefore = transform.eulerAngles;
+                rocketEulerBefore = t.rotation;
+                tempLauncherTransform.LookAt(targetTransform.position);
+                rocketEulerAfter = tempLauncherTransform.rotation;
+                launcherEulerAfter = new Vector3(270, tempLauncherTransform.eulerAngles.y, 0);
+                launchQuaternion = tempLauncherTransform.rotation;
+                tempLauncherTransform.eulerAngles = new Vector3(270, tempLauncherTransform.eulerAngles.y, 0);
+
+               
+                Debug.Log(rocketEulerBefore+"================"+rocketEulerAfter);
+                StartCoroutine(launchImpl());
+            }
+        }
+
+        protected IEnumerator launchImpl()
+        {
+            isLaunching = true;
+
+            float totalTime = 0.0f;
+
+            while (totalTime < 1.0f)
+            {
+                transform.eulerAngles = Vector3.Lerp(launcherEulerBefore, launcherEulerAfter, totalTime / 1.0f);
+                transform.Find("Rocket launcher").rotation = Quaternion.Lerp(rocketEulerBefore, rocketEulerAfter, totalTime / 1.0f);
+
+                totalTime += Time.deltaTime;
+                yield return new WaitForFixedUpdate();
+            }
+            transform.eulerAngles = launcherEulerAfter;
+            transform.Find("Rocket launcher").rotation = rocketEulerAfter;
+
+            Instantiate(missilePrefabs, rocketInitPos.position, launchQuaternion);
+
+            isLaunching = false;
+        }
+
+
+     
+    }
+}
