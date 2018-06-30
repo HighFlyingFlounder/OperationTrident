998e430300da5072e014861cea43ff02997b1157
CHINESE JIGE <836613859@qq.com>
2018-06-30 10:57:25 +0800

    谢幕的爆炸粒子特效定时销毁,etc.
    1.谢幕的爆炸粒子特效定时销毁
    2.改了下鲲模型的material，加了一堆normal map(在resource里)
    3.改了下天空球的材质

--- Assets/EndingScene/Scripts/ExplosionGenerator.cs
+++ Assets/EndingScene/Scripts/ExplosionGenerator.cs
@@ -27,14 +27,17 @@ namespace OperationTrident.EndingScene
             if (m_Time > m_CurrentTimeThrehold)
             {
                 //敌人在某个位置隔壁一定box范围内生成
-                Vector3 boxSize = new Vector3(700, 250, 350);
+                Vector3 boxSize = new Vector3(900, 500, 600);
                     float x_offset = Random.Range(-boxSize.x / 2.0f, boxSize.x / 2.0f);
                     float y_offset = Random.Range(-boxSize.y / 2.0f, boxSize.y / 2.0f);
                     float z_offset = Random.Range(-boxSize.z / 2.0f, boxSize.z / 2.0f);
-                    Instantiate(
+                    GameObject obj = Instantiate(
                         m_ExplosionPrefab,
                         m_KunTransform.position + new Vector3(x_offset, y_offset, z_offset),
                          m_KunTransform.rotation);
+
+                //过几秒删除爆炸的gameObject
+                StartCoroutine(DestroyExplosionPS(obj));
 
                 //重置一下爆炸的计时器
                 m_Time = 0.0f;
@@ -45,6 +48,12 @@ namespace OperationTrident.EndingScene
         public void SetExplodeMaxInterval(float maxInterval)
         {
             m_ExplodeMaxInterval = maxInterval;
+        }
+
+        private IEnumerator DestroyExplosionPS(GameObject obj)
+        {
+            yield return new WaitForSeconds(6.0f);
+            Destroy(obj);
         }
     }
 }
