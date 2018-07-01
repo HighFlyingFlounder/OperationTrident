ed8876c3ce362a975b4d9ef180bf0877676dd974
CHINESE JIGE <CHINA-JIGE@users.noreply.github.com>
2018-07-01 09:17:32 +0800

    淡入淡出(GUI的Utility) (#77)
    1.淡入淡出FadeIn/Out Utility（在Common/GUI, namespace OperationTrident.Util）
    2.太空光源改了下颜色
    3.进度条的GUI贴图改成用代码生成

--- Assets/EndingScene/Scripts/UniverseLightSourceFlare.cs
+++ Assets/EndingScene/Scripts/UniverseLightSourceFlare.cs
@@ -2,30 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UniverseLightSourceFlare : MonoBehaviour {
+namespace OperationTrident.EndingScene
+{
+    public class UniverseLightSourceFlare : MonoBehaviour
+    {
 
-    public LensFlare m_LensFlares;
+        public LensFlare m_LensFlares;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update() {
-        //在平行光的反向位置
-        //Vector3 offsetFromCam = -10000.0f * m_DirectionalLight.transform.forward;
-        //m_LensFlares.transform.position = Camera.current.transform.position + offsetFromCam;
-
-        //调一下LensFlares的亮度（正对着最亮）
-        Vector3 camLookat = Camera.main.transform.forward;
-        float dotP = Vector3.Dot(camLookat.normalized, -m_LensFlares.transform.forward.normalized);
-        if(dotP>0.0f)
+        // Use this for initialization
+        void Start()
         {
-            const float maxBrightness = 2.0f;
-            m_LensFlares.brightness = (1.0f - (Mathf.Acos(dotP)) / (Mathf.PI / 2.0f))*  maxBrightness;
+
         }
 
+        // Update is called once per frame
+        void Update()
+        {
+            //在平行光的反向位置
+            //Vector3 offsetFromCam = -10000.0f * m_DirectionalLight.transform.forward;
+            //m_LensFlares.transform.position = Camera.current.transform.position + offsetFromCam;
 
+            //调一下LensFlares的亮度（正对着最亮）
+            Vector3 camLookat = Camera.main.transform.forward;
+            float dotP = Vector3.Dot(camLookat.normalized, -m_LensFlares.transform.forward.normalized);
+            if (dotP > 0.0f)
+            {
+                const float maxBrightness = 2.0f;
+                m_LensFlares.brightness = (1.0f - (Mathf.Acos(dotP)) / (Mathf.PI / 2.0f)) * maxBrightness;
+            }
+
+
+        }
     }
 }
