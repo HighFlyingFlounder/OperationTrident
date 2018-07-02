3fdb3fe3994288387e73050a22a8d5a2cd4fbb7a
Peiwei Chang <windyicec@gmail.com>
2018-07-02 12:00:07 +0800

    开始界面GUI
    开始界面GUI，按钮淡入淡出，事件系统初成

---
+++ Assets/Room1Battle/StartGUI/StartSceneEvent.cs
@@ -0,0 +1,22 @@
+﻿using OperationTrident.EventSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace OperationTrident.StartScene {
+    public class StartSceneEvent : MonoBehaviour
+    {
+        public static readonly string GAME_START = "GAME_START";
+        public static readonly string HELP = "HELP";
+        public static readonly string GIVE_MONEY = "GIVE_MONEY";
+        public static readonly string EXIT_GAME = "EXIT_GAME";
+
+        /// <summary>
+        /// 开始游戏按钮回调
+        /// </summary>
+        public void OnGameStart()
+        {
+            Debug.Log("HonShu");
+            Messenger.Broadcast(GAME_START);
+        }
+    }
+}
