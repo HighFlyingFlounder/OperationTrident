fa8208bc59790b584a64aa22635e7c4c095ad68b
zekas0129 <62314742@qq.com>
2018-07-04 14:50:11 +0800

    暂存
    胜利后的分镜制作（未完成）

---
+++ Assets/SpaceBattle/TimeLines/FadeIn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OperationTrident.Util;
+
+public class FadeIn : MonoBehaviour {
+    FadeInOutUtil fade;
+    public float duration = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+        fade.FadeOut(duration, Color.black);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SpaceBattle/TimeLines/FadeOut.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OperationTrident.Util;
+
+public class FadeOut : MonoBehaviour {
+    FadeInOutUtil fade;
+    public float duration = 0.5f;
+    
+    void Start()
+    {
+        fade.FadeOut(duration, Color.black);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
