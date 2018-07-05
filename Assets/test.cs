f44e81f6aca4f2c1e3d180dfa44a549cb00a11c3
zekas0129 <62314742@qq.com>
2018-07-05 14:53:12 +0800

    把分镜整合到了游戏流程中

---
+++ Assets/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+    public GameObject timeLine;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            timeLine.SetActive(true);
+        }
+	}
+}
