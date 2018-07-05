cdff5fb3214289d788645152a2409bcf72cf55f2
DengJunYuan <540997806@qq.com>
2018-07-05 20:18:55 +0800

    Room1可以正常的同步死亡

--- Assets/Room1Battle/Scripts/DieHandler.cs
+++ Assets/Room1Battle/Scripts/DieHandler.cs
@@ -2,11 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using OperationTrident.EventSystem;
+using UnityEngine.UI;
+
+
 
 namespace OperationTrident.Room1
 {
-    public class DieHandler : MonoBehaviour
+    public class DieHandler : MonoBehaviour,NetSyncInterface
     {
+        private NetSyncController m_NetSyncController;
+        public const bool ture = true;
+        public readonly static string PLAYER_DIE = "PLAYER_DIE";
+
+        public float transparentSpeed = 0.01f;
+
+        public float maxTransparent = 0.75f;
+
+        [SerializeField]
+        private GameObject dieUIPrefab;
+
+        private void Awake()
+        {
+            Messenger.AddListener(PLAYER_DIE, DieHandlerFunc);
+        }
+
+        private void OnDestroy()
+        {
+            Messenger.RemoveListener(PLAYER_DIE, DieHandlerFunc);
+        }
+
+        void DieHandlerFunc()
+        {
+            m_NetSyncController.RPC(this, "DieHandlerImpl");
+            DieHandlerImpl();
+        }
+
+        public void DieHandlerImpl()
+        {
+            // 暂时不知道怎么拿到场景中的玩家
+            //GetComponent<FPS.Player.MovementController>().enabled = false;
+            //GetComponent<FPS.Player.MouseRotator>().enabled = false;
+
+            Instantiate(dieUIPrefab);
+
+
+            // 界面背景是黑色，开始透明
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().enabled = true;
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color = new Vector4(
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.r,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.g,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.b,
+                0.0f);
+            // 现实的文字是红色，开始透明
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().enabled = true;
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color = new Vector4(
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.r,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.g,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.b,
+                0.0f);
+            // 返回主界面的按钮加上回调
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Button>().enabled = true;
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Button>().onClick.AddListener(delegate () {
+                SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
+            });
+
+            // 按钮的颜色开始透明
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().enabled = true;
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color = new Vector4(
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.r,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.g,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.b,
+                0.0f);
+            // 按钮文字的颜色开始透明
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().enabled = true;
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color = new Vector4(
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.r,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.g,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.b,
+                0.0f
+                );
+        }
 
         public void OnReturnGameHallClick()
         {
@@ -20,10 +96,61 @@ namespace OperationTrident.Room1
 
         }
 
+        private void DieCanvasUpdate()
+        {
+            // 界面背景是黑色，开始透明
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color =
+                new Vector4(0.0f,
+                0.0f,
+                0.0f,
+                Mathf.Min(dieUIPrefab.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.a + transparentSpeed, maxTransparent));
+            // 现实的文字是红色，开始透明
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color = new Vector4(
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.r,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.g,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.b,
+                 Mathf.Min(dieUIPrefab.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.a+ transparentSpeed, maxTransparent));
+            // 返回主界面的按钮加上回调
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color = new Vector4(
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.r,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.g,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.b,
+                Mathf.Min(dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.a+transparentSpeed,maxTransparent));
+            // 按钮文字的颜色开始透明
+            dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color = new Vector4(
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.r,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.g,
+                dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.b,
+                Mathf.Min(dieUIPrefab.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.a+transparentSpeed,maxTransparent)
+                );
+        }
+
         // Update is called once per frame
         void Update()
         {
+            try
+            {
+                DieCanvasUpdate();
+            }
+            catch(System.Exception e)
+            {
 
+            }
+        }
+
+        public void RecvData(SyncData data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public SyncData SendData()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Init(NetSyncController controller)
+        {
+            m_NetSyncController = controller;
         }
     }
 }
--- Assets/Room1Battle/Scripts/ReactiveTarget.cs
+++ Assets/Room1Battle/Scripts/ReactiveTarget.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using OperationTrident.EventSystem;
 
 namespace OperationTrident.Room1
 {
@@ -34,11 +35,6 @@ namespace OperationTrident.Room1
             }
         }
 
-        private Camera mCamera;
-
-        [SerializeField]
-        private GameObject blackCanvas;
-
         // Use this for initialization
         void Start()
         {
@@ -48,63 +44,7 @@ namespace OperationTrident.Room1
         // Update is called once per frame
         void Update()
         {
-            if (dead)
-            {
-                DieCanvas();
-            }
-        }
 
-        private bool coroutineStart = false;
-
-        public void DieCanvas()
-        {
-            //blackCanvas.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().rectTransform. = blackCanvas.transform;
-            blackCanvas.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color =
-                new Vector4(0.0f,
-                0.0f,
-                0.0f,
-                Mathf.Min(blackCanvas.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.a + 0.01f, 0.75f));
-            blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().enabled = true;
-            blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color = new Vector4(
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.r,
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.g,
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.b,
-                 blackCanvas.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.a);
-            if (blackCanvas.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.a >= 0.75&&!coroutineStart)
-            {
-                coroutineStart = true;
-                StartCoroutine(DisplayButton());
-            }
-        }
-
-        IEnumerator DisplayButton()
-        {
-            yield return new WaitForSeconds(0.2f);
-            blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().enabled = true;
-            blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().enabled = true;
-            blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color = new Vector4(
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.r,
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.g,
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.b,
-                0.0f);
-            blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color = new Vector4(
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.r,
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.g,
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.b,
-                0.0f
-                );
-            while (blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.a<=1.0f) {
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color = new Vector4(
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.r,
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.g,
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.b,
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Image>().color.a + 0.01f);
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color = new Vector4(
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.r,
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.g,
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.b,
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").Find("Text").GetComponent<Text>().color.a + 0.01f);
-                    }
         }
 
         public void OnHit(string id,bool fromAI,int damage)
@@ -116,6 +56,8 @@ namespace OperationTrident.Room1
                 HitImplement(damage);
             else if (GameMgr.instance.id == id)//本地玩家造成伤害
             {
+                if (gameObject.name == id)//自己对自己不能造成的伤害
+                    return;
                 HitImplement(damage);
                 m_NetSyncController.RPC(this, "HitImplement", damage);
             }
@@ -153,30 +95,9 @@ namespace OperationTrident.Room1
             }
             else
             {
-                mCamera = GetComponent<FPS.Common.GetCamera>().GetMainCamera();
                 GetComponent<FPS.Player.MovementController>().enabled = false;
                 GetComponent<FPS.Player.MouseRotator>().enabled = false;
-                if (GameObject.Find("DieCanvas") == null)
-                {
-                    Debug.Log("0x11");
-                    Instantiate(blackCanvas);
-
-                }
-
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Button").GetComponent<Button>().onClick.AddListener(delegate () {
-                    SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
-                });
-                blackCanvas.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color = new Vector4(
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.r,
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.g,
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").GetComponent<Image>().color.b,
-                    0.0f);
-                blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color = new Vector4(
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.r,
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.g,
-                    blackCanvas.transform.Find("DieCanvas").Find("Image").Find("Text").GetComponent<Text>().color.b,
-                    0.0f);
-
+                Messenger.Broadcast(DieHandler.PLAYER_DIE);
             }
         }
 
