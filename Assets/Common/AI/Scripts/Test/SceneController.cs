8b8170fc21cb8187629f2110fc876ffc7d2216bc
ZezhongWang <904192625@qq.com>
2018-07-07 20:12:50 +0800

    网络同步大保健AI的生成，修改AIController

---
+++ Assets/Common/AI/Scripts/Test/SceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace OperationTrident.Common.AI
+{
+    public class SceneController : MonoBehaviour
+    {
+        [SerializeField]
+        WanderAIAgentInitParams[] wanderAIAgentInitParams;
+
+        // Use this for initialization
+        void Start()
+        {
+            AIController.instance.CreateAI(wanderAIAgentInitParams.Length, 0, "SwopPoints", wanderAIAgentInitParams);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
