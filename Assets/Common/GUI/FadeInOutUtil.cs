c18e11c38a804472fcd7ff9336d20dac5eab7c47
CHINESE JIGE <836613859@qq.com>
2018-07-08 10:21:24 +0800

    GUI小改
    1.FadeInOutUtil改成static函数，需要外部调用
    2.改了下StartGUI，中文标题，加了图标，然后加了字体

--- Assets/Common/GUI/FadeInOutUtil.cs
+++ Assets/Common/GUI/FadeInOutUtil.cs
@@ -5,74 +5,77 @@ using UnityEngine;
 namespace OperationTrident.Util
 {
     //请把FadeInOutUtil挂在游戏对象上，因为它要在OnGUI里面画一个贴图
-    public class FadeInOutUtil : MonoBehaviour
+    public static class FadeInOutUtil
     {
         //纯黑贴图
-        private Texture2D m_PureTexture;
+        private static Texture2D m_PureTexture=null;
 
         //全屏的矩形
-        private Rect m_FullScreenRect;
+        private static Rect m_FullScreenRect=Rect.zero;
 
         //现在处于哪个状态
-        private enum FADING_STATE
+        public enum FADING_STATE
         {
             FADING_OUT,
             FADING_IN,
             STOPPED
         };
 
-        private FADING_STATE m_FadingState=FADING_STATE.STOPPED;
-        private Color m_FadeColor;
-        private float m_Duration=0.0f;
-        private float m_CurrentFadingTime=0.0f;//当前的fade in/out已经多久了
+        private static FADING_STATE m_FadingState=FADING_STATE.STOPPED;
+        private static Color m_FadeColor=Color.black;
+        private static float m_Duration=0.0f;
+        private static float m_CurrentFadingTime=0.0f;//当前的fade in/out已经多久了
+        private static Camera m_GuiCamera=null;
 
         //Cam用来获取渲染窗口的的pixel rect
         //FadeColor是淡入/淡出的基本颜色，只有rgb有用
-        public void FadeIn(float duration,Color fadeColor)
+        //state是当前正在“淡入/淡出/停止"的状态
+        public static void SetFadingState(float duration,Camera guiCam,Color fadeColor,FADING_STATE state)
         {
-            this.enabled = true;
+            if(m_PureTexture==null)
+            {
+                m_PureTexture = new Texture2D(1, 1);
+                m_PureTexture.SetPixel(0, 0, Color.black);
+                m_PureTexture.Apply();
+            }
+
             m_CurrentFadingTime = 0.0f;
             m_Duration = duration;
+            m_GuiCamera = guiCam;
             m_FadeColor = fadeColor;
-            m_FadingState = FADING_STATE.FADING_IN;
+            m_FadingState = state;
         }
 
-        public void FadeOut(float duration, Color fadeColor)
+        public static FADING_STATE GetFadingState()
         {
-            this.enabled = true;
-            m_CurrentFadingTime = 0.0f;
-            m_Duration = duration;
-            m_FadeColor = fadeColor;
-            m_FadingState = FADING_STATE.FADING_OUT;
+            return m_FadingState;
         }
 
         /***********************************************
          *                     生命周期函数
          * *********************************************/
-        // Use this for initialization
-        void Start()
-        {
-            m_FullScreenRect = new Rect(new Vector2(0, 0), new Vector2(Screen.width, Screen.height));
-            m_FadeColor = Color.black;
-            m_PureTexture = new Texture2D(1, 1);
-            m_PureTexture.SetPixel(0, 0, Color.black);
-            m_PureTexture.Apply();
-        }
-
-        private void Update()
+         //！！！需要每帧在update里调用！！
+        public static void UpdateState()
         {
             m_CurrentFadingTime += Time.deltaTime;
             //时间够了就disable并且设置状态为STOPPED
             if(m_CurrentFadingTime>=m_Duration)
             {
                 m_FadingState = FADING_STATE.STOPPED;
-                this.enabled = false;
             }
         }
 
         //画一个纯色全屏贴图
-        void OnGUI()
+        //！！！需要每帧在OnGUI里调用！！
+
+        public static void RenderGUI()
         {
+            if (m_GuiCamera == null)
+            {
+                Debug.Log("warning: FadeInOutUtil GUI camera没绑定！");
+                return;
+            }
+
             Color c = m_FadeColor;
             switch (m_FadingState)
             {
@@ -82,7 +85,7 @@ namespace OperationTrident.Util
                         c.a = 1.0f - timeFactor* timeFactor;
                         m_PureTexture.SetPixel(0, 0, c);
                         m_PureTexture.Apply();
-                        GUI.DrawTexture(Camera.main.pixelRect, m_PureTexture, ScaleMode.StretchToFill, true);
+                        GUI.DrawTexture(m_GuiCamera.pixelRect, m_PureTexture, ScaleMode.StretchToFill, true);
                         break;
                     }
 
@@ -92,7 +95,7 @@ namespace OperationTrident.Util
                         c.a = timeFactor* timeFactor;
                         m_PureTexture.SetPixel(0, 0, c);
                         m_PureTexture.Apply();
-                        GUI.DrawTexture(Camera.main.pixelRect, m_PureTexture, ScaleMode.StretchToFill, true);
+                        GUI.DrawTexture(m_GuiCamera.pixelRect, m_PureTexture, ScaleMode.StretchToFill, true);
                         break;
                     }
 
