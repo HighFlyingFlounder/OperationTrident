40e6206e018303a6ee6aa07bad6d8e8a505c44b5
CHINESE JIGE <CHINA-JIGE@users.noreply.github.com>
2018-07-10 11:11:53 +0800

    Dev room5 ai (#151)
    * Room5 AI勉强加进去了 好爽啊
    
    * Room5地板改，AI生成小改，人物大小小改
    
    * 试一下Room5另外搞一个local player prefab
    
    * 前面的Commit不幸修改了NetworkPlayer.prefab并commit了，revert好像会把我之前的commit搞掉。现在我从远程仓库下了阿哥prefab下来再commit，有conflict请采用远端的修改

--- Assets/Room5Battle/Scripts/Subscenes/Subscene_CountDown.cs
+++ Assets/Room5Battle/Scripts/Subscenes/Subscene_CountDown.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using OperationTrident.Util;
+using OperationTrident.Common.AI;
 
 namespace OperationTrident.Room5
 {
@@ -10,6 +11,10 @@ namespace OperationTrident.Room5
         //反应柱开始冷却（变色）
         public TokamakReactorPillar m_ReactorPillar;
 
+        public WanderAIAgentInitParams[] wanderAIAgentInitParams;
+
+        public TurretActionController[] turretActionController;
+
         //敌人的prefab
         public GameObject m_EnemyPrefab1;
 
@@ -17,12 +22,12 @@ namespace OperationTrident.Room5
         public Transform[] m_EnemyGenPos = new Transform[4];
 
         //三分钟 180s
-        private float m_CountDownTime = 15.0f;
+        private float m_CountDownTime = 120.0f;
+        private float m_EnemySpawnDeltaTime = 20.0f;
 
         //BGM播放
         public AudioSource m_AudioSource;
         public AudioClip m_BGM_CountingDown;
-
 
         public override bool isTransitionTriggered()
         {
@@ -45,7 +50,12 @@ namespace OperationTrident.Room5
         public override void onSubsceneInit()
         {
             m_ReactorPillar.StartCoolDownProcedure();
-            StartCoroutine(spawnEnemies1());
+
+            for (int i = 0; i < (int)m_CountDownTime / m_EnemySpawnDeltaTime; ++i)
+            {
+                StartCoroutine(spawnEnemies1(i * m_EnemySpawnDeltaTime));
+            }
+
             StartCoroutine(spawnEnemies2());
 
             //开始倒计时，换BGM
@@ -69,19 +79,25 @@ namespace OperationTrident.Room5
             return minStr + ":" + secStr;
         } 
 
-        //生成第一波敌人
-        private IEnumerator spawnEnemies1()
+        //生成一小波敌人
+        private IEnumerator spawnEnemies1(float t)
         {
-            yield return new WaitForSeconds(5.0f);
-            for(int i=0;i<4;++i) EnemyGenerator.SpawnEnemy_ExactPos(m_EnemyPrefab1,m_EnemyGenPos[i]);
+            yield return new WaitForSeconds(t);
+            for (int i = 0; i < 2; ++i)
+            {
+                AIController.instance.CreateAI(4, 0, "AI-SpawnPositions", wanderAIAgentInitParams[0]);
+            }
+            //EnemyGenerator.SpawnEnemy_ExactPos(m_EnemyPrefab1,m_EnemyGenPos[i]);
         }
 
-        //生成第二波敌人
+        //生成一大波敌人
         private IEnumerator spawnEnemies2()
         {
-            yield return new WaitForSeconds(10.0f);
-            for (int i = 0; i < 4; ++i)
-                EnemyGenerator.SpawnEnemy_ExactPos(m_EnemyPrefab1, m_EnemyGenPos[i]);
+            yield return new WaitForSeconds(60.0f);
+            for (int i = 0; i < 2; ++i)
+            {
+                AIController.instance.CreateAI(4, 0, "AI-SpawnPositions", wanderAIAgentInitParams[0]);
+            }
         }
 
         /***************************************************
