c5888655a85e5c78393d259a2c2db8f817bb9f73
DengJunYuan <540997806@qq.com>
2018-07-11 10:05:03 +0800

    修复了太空站的玩家创建问题

--- Assets/Room1Battle/Scripts/Room1BattleNetManager.cs
+++ Assets/Room1Battle/Scripts/Room1BattleNetManager.cs
@@ -10,36 +10,5 @@ namespace OperationTrident.Room1
         {
             playerObj.AddComponent<InteractiveRay>();
         }
-
-        public void RecvResult(ProtocolBase protocol)
-        {
-            //解析协议
-            int start = 0;
-            ProtocolBytes proto = (ProtocolBytes)protocol;
-            string protoName = proto.GetString(start, ref start);
-            int isWin = proto.GetInt(start, ref start);
-            //弹出胜负面板
-            //玩家收到胜利条件后禁用掉list中的玩家
-            GameObject flyer;
-            Debug.Log("list.TryGetValue : " + list.TryGetValue(GameMgr.instance.id, out flyer));
-            if (list.TryGetValue(GameMgr.instance.id, out flyer))
-                flyer.SetActive(false);
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            //取消监听
-            NetMgr.srvConn.msgDist.DelListener("Result", RecvResult);
-            ClearBattle();
-       
-            if (isWin == 0)//失败
-            {
-                Debug.Log("Room1 Fail");
-                OperationTrident.EventSystem.Messenger.Broadcast(OperationTrident.Room1.DieHandler.PLAYER_DIE);
-            }
-            else//胜利
-            {
-                //某关卡胜利是直接进入下一个场景，故不会进入这里
-                Debug.Log("关卡胜利！");
-            }
-        }
     }
 }
