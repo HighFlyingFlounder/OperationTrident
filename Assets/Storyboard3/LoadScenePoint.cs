f7e19e4f88fe11cd06c3ac83c5fc990b1cf9ad40
DengJunYuan <540997806@qq.com>
2018-07-12 10:04:55 +0800

    现在过程动画中的人物数量能正确显示了
    sb2的最后加入了淡出效果

--- Assets/Storyboard3/LoadScenePoint.cs
+++ Assets/Storyboard3/LoadScenePoint.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using OperationTrident.Util;
 
 public class LoadScenePoint : MonoBehaviour {
     public string nextScene = "SpaceBattle";
+    private Camera m_FadeOutGuiCamera;
+    void Start()
+    {
+        m_FadeOutGuiCamera = null;
+    }
+    void Update()
+    {
+        FadeInOutUtil.UpdateState();
+    }
+    private void OnGUI()
+    {
+        FadeInOutUtil.RenderGUI();
+    }
+    public void SetGuiCamera(Camera cam)
+    {
+        m_FadeOutGuiCamera = cam;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+            //SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+            //淡出
+            float fadeOutDuration = 3.0f;
+            FadeInOutUtil.SetFadingState(fadeOutDuration, m_FadeOutGuiCamera, Color.black, FadeInOutUtil.FADING_STATE.FADING_OUT);
+            StartCoroutine(FadeOutCoroutine(fadeOutDuration));//等多一下确定淡出完成？
         }
+    }
+
+    private IEnumerator FadeOutCoroutine(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        GameMgr.instance.nextScene = nextScene;
+        SceneManager.LoadScene("Loading", LoadSceneMode.Single);
+        //SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
     }
 }
