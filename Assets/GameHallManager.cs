963ed72de85b3c0b58842c8b6fe853c5310153c8
DengJunYuan <540997806@qq.com>
2018-07-24 12:42:39 +0800

    死亡后退出游戏到房间，无法创建房间

--- Assets/GameHallManager.cs
+++ Assets/GameHallManager.cs
@@ -7,7 +7,10 @@ public class GameHallManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         if (PanelMgr.instance)//旧版本的UI系统
-            PanelMgr.instance.OpenPanel<StartPanel>("");
+            if(PanelMgr.login_in)
+                PanelMgr.instance.OpenPanel<RoomListPanel>("");
+            else PanelMgr.instance.OpenPanel<StartPanel>("");
+
     }
 	
 	// Update is called once per frame
